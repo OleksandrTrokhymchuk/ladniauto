69fe3619d9fe2b61d4852ca65d4fd5ef06593b62
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-20 23:38:48 +0300

    add basic footer and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <div className="flex flex-col min-h-[100%]">
+          <Header/>
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer/>
+        </div>
       </body>
     </html>
   );
