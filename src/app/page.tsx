529cb1c5b22aa0ab1f125f53d0c7f35af65214b7
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-29 18:58:49 +0300

    increase margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
     vsm:pt-32 md:pt-48
     pb-20"
         style={{ backgroundImage: "url(/images/hero-background.jpg)" }}>
-          <div className="custom-container vsm:text-base lg:text-lg">
+          <div className="custom-container vsm:text-lg lg:text-xl">
             <h1
-              className="vsm:text-6xl lg:text-7xl font-bold text-project-blue mb-5"
+              className="vsm:text-6xl lg:text-7xl font-bold text-project-blue mb-10"
             >
               Авто із США під ключ
             </h1>
-            <div className="flex flex-col gap-y-4 mb-12">
+            <div className="flex flex-col gap-y-6 mb-16">
               <h2>Ми не продаємо авто – ми продаємо спокій. Реалізовуємо автомобільні мрії клієнтів</h2>
               <p>Від аукціону США до видачі відновленого авто у вашому місті. Купуємо безпечні, а віддаємо готові. Ви обираєте – ми робимо результат. Чесно. Прозоро. Без зайвих турбот</p>
             </div>
-            <div className="mb-5">
+            <div className="mb-8">
               <p>
                 Залишайте заявку на персональну консультацію та безкоштовний підбір!
               </p>
