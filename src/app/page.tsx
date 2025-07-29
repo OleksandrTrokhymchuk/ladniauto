19cfac4e89a812c37885e5adfe75e28cbc5ffa3b
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-29 19:01:15 +0300

    modify hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,29 @@ export default function Home() {
     pb-20"
         style={{ backgroundImage: "url(/images/hero-background.jpg)" }}>
           <div className="custom-container vsm:text-lg lg:text-xl">
-            <h1
-              className="vsm:text-6xl lg:text-7xl font-bold text-project-blue mb-10"
-            >
-              Авто із США під ключ
-            </h1>
-            <div className="flex flex-col gap-y-6 mb-16">
-              <h2>Ми не продаємо авто – ми продаємо спокій. Реалізовуємо автомобільні мрії клієнтів</h2>
-              <p>Від аукціону США до видачі відновленого авто у вашому місті. Купуємо безпечні, а віддаємо готові. Ви обираєте – ми робимо результат. Чесно. Прозоро. Без зайвих турбот</p>
-            </div>
-            <div className="mb-8">
-              <p>
-                Залишайте заявку на персональну консультацію та безкоштовний підбір!
-              </p>
-              <button
-                className="bg-project-green mt-5 rounded-full
-                py-2 px-5
-                transition-all duration-500
-                hover:scale-110 hover:opacity-85"
+            <div className="min-h-screen">
+              <h1
+                className="vsm:text-6xl lg:text-7xl font-bold text-project-blue mb-10"
               >
-                Залишити заявку
-              </button>
+                Авто із США під ключ
+              </h1>
+              <div className="flex flex-col gap-y-6 mb-16">
+                <h2>Ми не продаємо авто – ми продаємо спокій. Реалізовуємо автомобільні мрії клієнтів</h2>
+                <p>Від аукціону США до видачі відновленого авто у вашому місті. Купуємо безпечні, а віддаємо готові. Ви обираєте – ми робимо результат. Чесно. Прозоро. Без зайвих турбот</p>
+              </div>
+              <div className="mb-8">
+                <p>
+                  Залишайте заявку на персональну консультацію та безкоштовний підбір!
+                </p>
+                <button
+                  className="bg-project-green mt-5 rounded-full
+                  py-2 px-5
+                  transition-all duration-500
+                  hover:scale-110 hover:opacity-85"
+                >
+                  Залишити заявку
+                </button>
+              </div>
             </div>
             <div className="bg-project-blue w-full rounded-3xl p-5 bg-opacity-90 shadow-2xl">
               <p className="text-center text-4xl font-bold">Оберіть автомобіль</p>
