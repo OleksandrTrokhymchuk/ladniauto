dbc9024119d792aaa0e158f6d4184e00f76256e5
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-08-06 14:57:33 +0300

    hero with bg1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Hero from "./components/Hero"
-import heroBackground from "../../public/images/bg1.png"
+import heroBackground from "../../public/images/bg2.png"
 
 export default function Home() {
   return (
