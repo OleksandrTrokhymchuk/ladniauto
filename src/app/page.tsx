0754fe8fa562d9e7e4d39d8fceaa43dd4baa1f62
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-08-12 20:30:33 +0300

    modify hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import heroBackground from "../../public/images/hero-background2.jpg";
+import heroBackgroundMobile from "../../public/images/hero-backgorund-mobile.jpg";
 import Services from "./components/Services";
 import WhyUs from "./components/WhyUs";
 
@@ -8,18 +9,34 @@ export default function Home() {
   return (
     <>
       <section className="relative min-h-screen text-project-white vsm:pt-32 md:pt-64 pb-20">
-        <Image
-          src={heroBackground}
-          alt="Hero Background"
-          priority
-          quality={100}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: "cover",
-            objectPosition: "bottom right",
-          }}
-        />
+        <div className="hidden md:block">
+          <Image
+            src={heroBackground}
+            alt="Hero Background"
+            priority
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+              objectPosition: "bottom right",
+            }}
+          />
+        </div>
+        <div className="md:hidden">
+          <Image
+            src={heroBackgroundMobile}
+            alt="Hero Background"
+            priority
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+              objectPosition: "bottom left",
+            }}
+          />
+        </div>
         <div className="relative">
           <Hero />
         </div>
